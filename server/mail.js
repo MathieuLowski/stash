e103b1dcc608066213a680803208715f6d6805f0
Mathieu Szydlowski <mathieu.lowski@gmail.com>
2021-03-22 14:32:42 -0400

    fixed untracked files

--- server/mail.js
+++ server/mail.js
@@ -1,29 +1,28 @@
 const nodemailer = require("nodemailer");
 const mailGun = require("nodemailer-mailgun-transport");
+const { AUTH_API_KEY, DOMAIN } = require("./config/keys");
 
-const auth ={
-    auth:{
-        api_key:"4b8de68b1a45dba6bacd280946cfbdb2-53c13666-37a43133",
-        domain:"sandboxf91a8fe8b21044608cf2b2db43737766.mailgun.org"
-    }
+const auth = {
+  auth: {
+    api_key: AUTH_API_KEY,
+    domain: DOMAIN,
+  },
 };
 
 const transporter = nodemailer.createTransport(mailGun(auth));
 
-const mailOptions= {
-    from:"mathieu.lowski@gmail.com",
-    to:"rudy102dev@gmail.com",
-    subject:"test mail",
-    text:"Hopefully it won't take to much time !!"
+const mailOptions = {
+  from: "mathieu.lowski@gmail.com",
+  to: "rudy102dev@gmail.com",
+  subject: "test mail",
+  text: "Hopefully it won't take to much time !!",
 };
 
-transporter.sendMail(mailOptions, function(error, data){
-    if (error) {
-        console.log("Error is",error);
-        } else {
-          console.log('Email sent: ' + data.response);
-          res
-            .status(200)
-            .json({ status: 200, message: "Message succesfully sent." });
-        }
-})
+transporter.sendMail(mailOptions, function (error, data) {
+  if (error) {
+    console.log("Error is", error);
+  } else {
+    console.log("Email sent: " + data.response);
+    res.status(200).json({ status: 200, message: "Message succesfully sent." });
+  }
+});
